816fa99c780b53bdaadfd27bd49ce8f5bc7ac97f
David Thomas <dave.a.base@gmail.com>
2023-03-17 17:50:07 -0500

    file size fix

--- server/app.js
+++ server/app.js
@@ -23,9 +23,9 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '800mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '800mb' }));
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json({ limit: '800mb' }));
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '800mb', extended: true }));
 
 // set root to serve client app
 app.use('/', express.static('./pages/'));
